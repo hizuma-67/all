27e1757350b8073cfe4c79fe9cb020af839a1b3e
hizuma_67 <bbuichihieu@gmail.com>
2026-02-18 21:55:07 +0700

    Update and rename 6_7hub.vn.lua to 6_7hub.lua

---
+++ 6_7hub.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hizuma-67/all/refs/heads/main/3_6hub.obfuscated.lua.txt"))()
--- 6_7hub.vn.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://github.com/hizuma-67/all/blob/main/3_76_7.obfuscated-1.lua.txt"))()
